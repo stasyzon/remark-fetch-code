cdbe8762ec84d065bb8da7762b204ea06c6d2c62
Stanislav <stasizon@gmail.com>
2021-06-07 11:08:45 +0300

    Add removing unnecessary spaces if tag used

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -7,59 +7,59 @@ import remark from 'remark';
 const {Response} = jest.requireActual('node-fetch');
 const mockedFetch = <jest.Mock<typeof Response>><unknown>fetch;
 
-// describe('Basic checks', () => {
-//   beforeEach(() => mockedFetch.mockReturnValueOnce(Promise.resolve(new Response())));
-//   afterEach(() => mockedFetch.mockReset());
-//
-//   test('should call fetch if source have code type node with url metadata', async () => {
-//     await remark().use(remarkFetchCode).process('```js url=example.com\n```');
-//
-//     expect(mockedFetch).toBeCalled();
-//   })
-//
-//   test('should not call fetch if node does not contain meta', async () => {
-//     await remark().use(remarkFetchCode).process('```js\n```');
-//
-//     expect(mockedFetch).not.toBeCalled();
-//   })
-//
-//   test('should not call fetch if node does not contain url in meta', async () => {
-//     await remark().use(remarkFetchCode).process('```js something=example.com\n```');
-//
-//     expect(mockedFetch).not.toBeCalled();
-//   })
-// });
-//
-// describe('Options checks', () => {
-//   beforeEach(() => mockedFetch.mockReturnValueOnce(Promise.resolve(new Response())));
-//   afterEach(() => mockedFetch.mockReset());
-//
-//   describe('pathPrefix', function () {
-//     test('should form a path if pathPrefix is domain', async () => {
-//       await remark()
-//         .use(remarkFetchCode, {pathPrefix: 'https://example.com'})
-//         .process('```js url=/path/to/code.js\n```');
-//
-//       expect(mockedFetch).toBeCalledWith('https://example.com/path/to/code.js');
-//     })
-//
-//     test('should form a path if pathPrefix is domain with / on end', async () => {
-//       await remark()
-//         .use(remarkFetchCode, {pathPrefix: 'https://example.com/'})
-//         .process('```js url=/path/to/code.js\n```');
-//
-//       expect(mockedFetch).toBeCalledWith('https://example.com/path/to/code.js');
-//     })
-//
-//     test('should form a path if pathPrefix domain with pathname', async () => {
-//       await remark()
-//         .use(remarkFetchCode, {pathPrefix: 'https://example.com/example/'})
-//         .process('```js url=/path/to/code.js\n```');
-//
-//       expect(mockedFetch).toBeCalledWith('https://example.com/example/path/to/code.js');
-//     })
-//   });
-// });
+describe('Basic checks', () => {
+  beforeEach(() => mockedFetch.mockReturnValueOnce(Promise.resolve(new Response())));
+  afterEach(() => mockedFetch.mockReset());
+
+  test('should call fetch if source have code type node with url metadata', async () => {
+    await remark().use(remarkFetchCode).process('```js url=example.com\n```');
+
+    expect(mockedFetch).toBeCalled();
+  })
+
+  test('should not call fetch if node does not contain meta', async () => {
+    await remark().use(remarkFetchCode).process('```js\n```');
+
+    expect(mockedFetch).not.toBeCalled();
+  })
+
+  test('should not call fetch if node does not contain url in meta', async () => {
+    await remark().use(remarkFetchCode).process('```js something=example.com\n```');
+
+    expect(mockedFetch).not.toBeCalled();
+  })
+});
+
+describe('Options checks', () => {
+  beforeEach(() => mockedFetch.mockReturnValueOnce(Promise.resolve(new Response())));
+  afterEach(() => mockedFetch.mockReset());
+
+  describe('pathPrefix', function () {
+    test('should form a path if pathPrefix is domain', async () => {
+      await remark()
+        .use(remarkFetchCode, {pathPrefix: 'https://example.com'})
+        .process('```js url=/path/to/code.js\n```');
+
+      expect(mockedFetch).toBeCalledWith('https://example.com/path/to/code.js');
+    })
+
+    test('should form a path if pathPrefix is domain with / on end', async () => {
+      await remark()
+        .use(remarkFetchCode, {pathPrefix: 'https://example.com/'})
+        .process('```js url=/path/to/code.js\n```');
+
+      expect(mockedFetch).toBeCalledWith('https://example.com/path/to/code.js');
+    })
+
+    test('should form a path if pathPrefix domain with pathname', async () => {
+      await remark()
+        .use(remarkFetchCode, {pathPrefix: 'https://example.com/example/'})
+        .process('```js url=/path/to/code.js\n```');
+
+      expect(mockedFetch).toBeCalledWith('https://example.com/example/path/to/code.js');
+    })
+  });
+});
 
 describe('Transform checks', () => {
   afterEach(() => mockedFetch.mockReset());
@@ -92,10 +92,10 @@ describe('Transform checks', () => {
 
     const result = await remark()
       .use(remarkFetchCode)
-      .process('```js url=/path/to/code.js\n```');
+      .process('```js url=/path/to/code.js tag=REGION_1\n```');
 
     expect(result.contents).toEqual(
-      'ASAS'
+      '```js url=/path/to/code.js tag=REGION_1\nconst a = 1\n\tconst b = 1\n```\n'
     );
   })
 });
--- src/utils/extractTagSection.ts
+++ src/utils/extractTagSection.ts
@@ -1,4 +1,4 @@
-import { EOL } from 'os';
+import {EOL} from 'os';
 
 export default function extractTagSection(content: string, codeTag?: string) {
   if (!codeTag) {
@@ -6,14 +6,15 @@ export default function extractTagSection(content: string, codeTag?: string) {
   }
 
   const lines = content.split(EOL);
+  const tagFragmentLines = getTagSection(lines, codeTag);
+  const formattedLines = removeNonSpaceOnStart(tagFragmentLines);
+  return formattedLines.join(EOL);
+}
+
+function getTagSection(lines: string[], codeTag: string): string[] {
   let indexOfStartString = 0;
   let indexOfLastString = lines.length;
-  let spaceCountToRemove = 0;
   lines.forEach((line, index) => {
-    const spaceMatchOnStringStart = line.match(/^\s+/);
-    if (spaceMatchOnStringStart && spaceMatchOnStringStart.length < spaceCountToRemove) {
-      spaceCountToRemove = spaceMatchOnStringStart.length;
-    }
     if (line.includes(`START ${codeTag}`)) {
       indexOfStartString = index + 1;
     }
@@ -23,5 +24,28 @@ export default function extractTagSection(content: string, codeTag?: string) {
     }
   });
 
-  return lines.slice(indexOfStartString, indexOfLastString).join('\n');
+  return lines.slice(indexOfStartString, indexOfLastString);
+}
+
+function removeNonSpaceOnStart(lines: string[]): string[] {
+  let minimumSpaceCharacters: number | undefined;
+
+  lines.forEach(line => {
+    const spaceMatchOnStringStart = line.match(/^\s+/);
+
+    if (!spaceMatchOnStringStart || !spaceMatchOnStringStart[0]) {
+      minimumSpaceCharacters = 0;
+      return;
+    }
+
+    if (minimumSpaceCharacters === undefined) {
+      minimumSpaceCharacters = spaceMatchOnStringStart[0].length;
+    }
+
+    if (spaceMatchOnStringStart[0].length > 0 && spaceMatchOnStringStart[0].length < minimumSpaceCharacters) {
+      minimumSpaceCharacters = spaceMatchOnStringStart[0].length;
+    }
+  });
+
+  return lines.map(line => line.slice(minimumSpaceCharacters))
 }
